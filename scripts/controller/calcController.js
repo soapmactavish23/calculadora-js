f5a706baf1cf8b44b66467376d91ef1c64cf5671
HKprogram <ricknogueira1231@gmail.com>
2021-06-03 09:54:01 -0300

    Manipulando a DOM

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,7 +2,20 @@ class CalcController {
 
     constructor(){
         this._displayCalc = "0";
-        this._dataAtual;
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let timeEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "10";
+        dateEl.innerHTML = "01/05/2021";
+        timeEl.innerHTML = "00:00";
+
     }
 
     get displayCalc(){
@@ -13,11 +26,11 @@ class CalcController {
         this._displayCalc = valor;
     }
 
-    get dataAtual(){
-        return this._dataAtual;
+    get currentDate(){
+        return this._currentDate;
     }
     
-    set dataAtual(valor){
+    set currentDate(valor){
         this.dataAtual = valor;
     }
 
