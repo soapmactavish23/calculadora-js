b7f584fe313a34d5ecade5f6bcb176d39b550144
HKprogram <ricknogueira1231@gmail.com>
2021-06-07 17:48:53 -0300

    Adicionando o isNaN

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -33,8 +33,35 @@ class CalcController {
         this._operation.pop();
     }
 
+    getLastOperation() {
+        return this._operation[this._operation.length - 1];
+    }
+
+    setLastOperation(value){
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    isOperator(value) {
+        return (['+', '-', '%', '*', '/'].indexOf(value) > -1);
+    }
+
     addOperation(value) {
-        this._operation.push(value);
+        console.log('A', isNaN(this.getLastOperation()));
+        if (isNaN(this.getLastOperation())) {
+            if (this.isOperator(value)) {
+                this.setLastOperation(value);
+            } else if (isNaN(value)) {
+
+                console.log(value);
+
+            } else {
+                this._operation.push(value);
+            }
+        } else {
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+        }
+        
         console.log(this._operation);
     }
 
@@ -51,20 +78,24 @@ class CalcController {
                 this.clearEntry();
                 break;
             case 'porcento':
+                this.addOperation('%');
                 break;
             case 'divisao':
-                break;
-            case 'divisao':
+                this.addOperation('/');
                 break;
             case 'subtracao':
+                this.addOperation('-');
                 break;
             case 'soma':
-                break;
-            case 'igual':
+                this.addOperation('+');
                 break;
             case 'ponto':
+                this.addOperation('.');
                 break;
             case 'multiplicacao':
+                this.addOperation('*');
+                break;
+            case 'igual':
                 break;
             case '0':
             case '1':
