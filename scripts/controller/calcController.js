fa374fe8b2347cfd30f028ed69a3c926d8fe1441
HKprogram <ricknogueira1231@gmail.com>
2021-06-08 16:14:00 -0300

    Eval - Validando o primeiro operador e calculando

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -37,7 +37,7 @@ class CalcController {
         return this._operation[this._operation.length - 1];
     }
 
-    setLastOperation(value){
+    setLastOperation(value) {
         this._operation[this._operation.length - 1] = value;
     }
 
@@ -45,24 +45,53 @@ class CalcController {
         return (['+', '-', '%', '*', '/'].indexOf(value) > -1);
     }
 
+    pushOperation(value) {
+
+        this._operation.push(value);
+
+        if (this._operation.length > 3) {
+            this.calc();
+        }
+
+    }
+
+    calc() {
+
+        let last = this._operation.pop();
+        let result = eval(this._operation.join(''));
+
+        this._operation = [result, last];
+
+    }
+
+    setLastNumberToDisplay() {
+        
+    }
+
     addOperation(value) {
-        console.log('A', isNaN(this.getLastOperation()));
         if (isNaN(this.getLastOperation())) {
             if (this.isOperator(value)) {
                 this.setLastOperation(value);
             } else if (isNaN(value)) {
 
-                console.log(value);
-
             } else {
-                this._operation.push(value);
+                this.pushOperation(value);
             }
         } else {
-            let newValue = this.getLastOperation().toString() + value.toString();
-            this.setLastOperation(parseInt(newValue));
+
+            if (this.isOperator(value)) {
+
+                this.pushOperation(value);
+
+            } else {
+
+                let newValue = this.getLastOperation().toString() + value.toString();
+                this.setLastOperation(parseInt(newValue));
+
+                this.setLastNumberToDisplay();
+
+            }
         }
-        
-        console.log(this._operation);
     }
 
     setError() {
