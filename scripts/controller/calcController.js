ff805b89b5bd2bdca9aee3ce51434c2a41ef4b5e
HKprogram <ricknogueira1231@gmail.com>
2021-08-15 18:31:25 -0300

    Trabalhando com áudio

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor() {
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
@@ -33,7 +35,7 @@ class CalcController {
     pasteFromClipboard() {
 
         document.addEventListener("paste", e => {
-            
+
             let text = e.clipboardData.getData('Text');
 
             this.displayCalc = parseFloat(text);
@@ -58,10 +60,30 @@ class CalcController {
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
 
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+            btn.addEventListener('dblclick', e => {
+                this.toggleAudio();
+            })
+        })
+
+    }
+
+    toggleAudio() {
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio() {
+        if (this._audioOnOff) {
+            this._audio.currentTime = 0;
+            this._audio.play();
+        }
     }
 
     initKeyboard() {
         document.addEventListener('keyup', e => {
+
+            this.playAudio();
+
             switch (e.key) {
 
                 case 'Escape':
@@ -281,6 +303,8 @@ class CalcController {
     }
 
     execBtn(value) {
+
+        this.playAudio();
 
         switch (value) {
 
